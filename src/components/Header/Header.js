bcba81b3701e1d9628b977aa1b3eef2565d715d3
jovan6jovan <jovan6jovanovic@gmail.com>
2020-04-05 00:48:05 +0200

    Header and Landing for mobile done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,22 +3,30 @@ import { Link } from "react-router-dom";
 import "./Header.scss";
 
 const Header = () => (
-    <header>
-        <Link to="/" className="logo">Jovan Jovanovic</Link>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/projects">Projects</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
+  <header>
+    <Link to="/" className="logo">
+      jovan6jovan
+    </Link>
+    <nav className="nav">
+      <ul className="nav__list">
+        <li className="nav__list-item">
+          <Link to="/about" className="nav__list-item-link">
+            About
+          </Link>
+        </li>
+        <li className="nav__list-item">
+          <Link to="/projects" className="nav__list-item-link">
+            Projects
+          </Link>
+        </li>
+        <li className="nav__list-item">
+          <Link to="/contact" className="nav__list-item-link">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 );
 
 export default Header;
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Landing.scss";
+import programmer from "../../images/programmer.svg"
 
 const Landing = () => (
-    <div>Hi from landing</div>
+  <main className="landing">
+    <section className="landing__content">
+      <h1 className="landing__title">Front-End Developer</h1>
+      <p className="landing__text">
+        Not that we aren’t huge fans of IKEA, of course. I do think this
+        combination can work, but it needs to be used as an accent, rather than a
+        main color. In this space from Femina, the blue and yellow combo is lively
+        but not childish when used in a judicious way.
+      </p>
+      <Link to="/projects" className="btn btn--blue landing__btn">
+        Explore my projects
+      </Link>
+    </section>
+    <section className="landing__img">
+      <img src={programmer} alt="Programmer illustration" />
+    </section>
+  </main>
 );
 
 export default Landing;
