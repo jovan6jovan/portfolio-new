0def30981ceb5b71ab81b7e99d3de3555fe9d95e
jovan6jovan <jovan6jovanovic@gmail.com>
2020-05-01 23:12:29 +0200

    Started to implement contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,9 +2,35 @@ import React from "react";
 import "./Contact.scss";
 
 const Contact = () => (
-    <div>
-        Hi from contact
-    </div>
+  <section className="contact">
+    <h2>Feel free to contact me!</h2>
+    <form
+      className="contact__form"
+      name="contact"
+      method="POST"
+      data-netlify="true"
+    >
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label htmlFor="name">Your Name*: </label> <br />
+        <input type="text" name="name" id="name" min="3" required />
+      </p>
+      <p>
+        <label htmlFor="email">Your Email*: </label> <br />
+        <input type="email" name="email" id="email" required />
+      </p>
+      <p>
+        <label htmlFor="message">Message*: </label> <br />
+        <textarea name="message" id="message" required></textarea>
+      </p>
+      <p>
+        <div data-netlify-recaptha="true"></div>
+      </p>
+      <p>
+        <input className="btn btn--blue contact__btn" type="submit" value="SEND" />
+      </p>
+    </form>
+  </section>
 );
 
 export default Contact;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,31 +2,44 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.scss";
 
-const Header = () => (
-  <header>
-    <Link to="/" className="logo">
-      jovan6jovan
-    </Link>
-    <nav className="nav">
-      <ul className="nav__list">
+const Header = () => {
+  const onNavItemClick = (e) => {
+    let current = document.getElementsByClassName("active-nav-item");
+    current[0].className = current[0].className.replace(" active-nav-item", "");
+    e.target.className += " active-nav-item";
+  };
+
+  return (
+    <header>
+      <Link to="/" className="logo">
+        jovan6jovan
+      </Link>
+      <nav className="nav">
+        <ul className="nav__list">
         <li className="nav__list-item">
-          <Link to="/about" className="nav__list-item-link">
-            About
-          </Link>
-        </li>
-        <li className="nav__list-item">
-          <Link to="/projects" className="nav__list-item-link">
-            Projects
-          </Link>
-        </li>
-        <li className="nav__list-item">
-          <Link to="/contact" className="nav__list-item-link">
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+            <Link to="/" className="nav__list-item-link active-nav-item" onClick={onNavItemClick}>
+              Home
+            </Link>
+          </li>
+          <li className="nav__list-item">
+            <Link to="/about" className="nav__list-item-link" onClick={onNavItemClick}>
+              About
+            </Link>
+          </li>
+          <li className="nav__list-item">
+            <Link to="/projects" className="nav__list-item-link" onClick={onNavItemClick}>
+              Projects
+            </Link>
+          </li>
+          <li className="nav__list-item">
+            <Link to="/contact" className="nav__list-item-link" onClick={onNavItemClick}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
