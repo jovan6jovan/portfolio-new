cd436d3a89d3897585b4ec8491a689fd425eb91e
jovan6jovan <jovan6jovanovic@gmail.com>
2020-05-04 14:05:59 +0200

    Production test

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,6 @@ import { Link } from "react-router-dom";
 import "./Header.scss";
 
 const Header = () => {
-  const onNavItemClick = (e) => {
-    let current = document.getElementsByClassName("active-nav-item");
-    current[0].className = current[0].className.replace(" active-nav-item", "");
-    e.target.className += " active-nav-item";
-  };
-
   return (
     <header>
       <Link to="/" className="logo">
@@ -16,23 +10,18 @@ const Header = () => {
       </Link>
       <nav className="nav">
         <ul className="nav__list">
-        <li className="nav__list__item">
-            <Link to="/" className="nav__list__item__link active-nav-item" onClick={onNavItemClick}>
-              Home
-            </Link>
-          </li>
           <li className="nav__list__item">
-            <Link to="/about" className="nav__list__item__link" onClick={onNavItemClick}>
+            <Link to="/about" className="nav__list__item__link">
               About
             </Link>
           </li>
           <li className="nav__list__item">
-            <Link to="/projects" className="nav__list__item__link" onClick={onNavItemClick}>
+            <Link to="/projects" className="nav__list__item__link">
               Projects
             </Link>
           </li>
           <li className="nav__list__item">
-            <Link to="/contact" className="nav__list__item__link" onClick={onNavItemClick}>
+            <Link to="/contact" className="nav__list__item__link">
               Contact
             </Link>
           </li>
